87e7359607be24ea63aca094eeb0c8b0c75579a5
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-05-07 00:16:05 +0530

    Update ShizukuHelper.kt

--- app/src/main/java/com/asgfx/bgmi/utils/ShizukuHelper.kt
+++ app/src/main/java/com/asgfx/bgmi/utils/ShizukuHelper.kt
@@ -1,12 +1,18 @@
 package com.asgfx.bgmi.utils
 
 import rikka.shizuku.Shizuku
+import java.io.OutputStream
 
 object ShizukuHelper {
     fun executeShell(command: String): Boolean {
         return try {
             if (Shizuku.pingBinder()) {
-                val process = Shizuku.newProcess(arrayOf("sh", "-c", command), null, null)
+                // Shizuku ke through process chalane ka sahi tareeka
+                val process = Shizuku.newProcess(arrayOf("sh"), null, null)
+                val os: OutputStream = process.outputStream
+                os.write((command + "\n").toByteArray())
+                os.write("exit\n".toByteArray())
+                os.flush()
                 process.waitFor()
                 true
             } else false
