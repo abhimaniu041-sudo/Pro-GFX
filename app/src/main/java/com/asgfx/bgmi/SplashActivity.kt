601fb6c23c6ef9e18a2746bfcbec4d4f1bc9b9ed
abhimaniu041-sudo <abhimaniu041@gmail.com>
2026-05-07 22:57:15 +0530

    Update SplashActivity.kt

--- app/src/main/java/com/asgfx/bgmi/SplashActivity.kt
+++ app/src/main/java/com/asgfx/bgmi/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.asgfx.bgmi
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -16,9 +17,22 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 2 Seconds ke baad LoginActivity par le jayega
+        // 2 Seconds ka wait
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+            
+            // 🔥 SESSION CHECK: Kya user pehle login kar chuka hai?
+            val sharedPref = getSharedPreferences("UserSession", Context.MODE_PRIVATE)
+            val isLoggedIn = sharedPref.getBoolean("isLoggedIn", false)
+
+            if (isLoggedIn) {
+                // Seedha Dashboard par jao aur naam pass karo
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("USER_NAME", sharedPref.getString("USER_NAME", "Pro User"))
+                startActivity(intent)
+            } else {
+                // Agar login nahi hai toh Login page dikhao
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
             finish()
         }, 2000)
     }
